e2816c93f0447f1101c4bcf175b86cdb590c2225
Abdullah <abdullah.mohamed1047@gmail.com>
2023-10-03 17:53:56 +0300

    Add chrome headless in our project

--- src/test/java/Tests/TestBase.java
+++ src/test/java/Tests/TestBase.java
@@ -4,8 +4,12 @@ import Utilities.Helper;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.ITestResult;
 import org.testng.annotations.*;
 
@@ -20,10 +24,19 @@ public class TestBase extends AbstractTestNGCucumberTests {
         if(browserName.equalsIgnoreCase( "chrome"))
         {
             driver =new ChromeDriver();
-        } else if (browserName.equalsIgnoreCase("firefox")) {
+        }
+        else if (browserName.equalsIgnoreCase("firefox")) {
             driver = new FirefoxDriver();
         }else if (browserName.equalsIgnoreCase("edge")){
             driver = new EdgeDriver();
+        }
+        // Chrome headless
+        else if(browserName.equalsIgnoreCase("chrome-headless"))
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless");
+            options.addArguments("--window-size=1920,1080");
+            driver = new ChromeDriver(options);
         }
         driver.manage().window().maximize();
         driver.get("https://demo.nopcommerce.com/");
